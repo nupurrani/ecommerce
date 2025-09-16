3d1dba58ac32166bfabb91bc8ff63b4d1effa747
nupurrani <128977360+nupurrani@users.noreply.github.com>
2025-09-16 16:45:41 +0530

    n

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,38 +1,57 @@
 import React from "react";
 import { Col, Dropdown, Row, Container, Card, Button } from "react-bootstrap";
-import Table from "react-bootstrap/Table";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
 const Address = () => {
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+  });
   return (
     <div>
       <section>
         <Container>
           <Row>
             <Col>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Sl. no.</th>
-                    <th>Address</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>C-125, Sector-1, Rohini, Delhi -110085</td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>
-                      FF09, Global Foyer Mall, Palam Vihar, Gurugram-122017
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>3</td>
-                    <td>kadma,jamshedpur-831001 </td>
-                    {/* <td>@twitter</td> */}
-                  </tr>
-                </tbody>
-              </Table>
+              <h1>Signup</h1>
+              <Formik
+                initialValues={{
+                  firstName: "",
+                  lastName: "",
+                  email: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={(values) => {
+                  // same shape as initial values
+                  console.log(values);
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <Field name="firstName" />
+                    {errors.firstName && touched.firstName ? (
+                      <div>{errors.firstName}</div>
+                    ) : null}
+                    <Field name="lastName" />
+                    {errors.lastName && touched.lastName ? (
+                      <div>{errors.lastName}</div>
+                    ) : null}
+                    <Field name="email" type="email" />
+                    {errors.email && touched.email ? (
+                      <div>{errors.email}</div>
+                    ) : null}
+                    <button type="submit">Submit</button>
+                  </Form>
+                )}
+              </Formik>
             </Col>
           </Row>
         </Container>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,99 @@
-import React from 'react'
+import React from "react";
 import { Col, Dropdown, Row, Container, Card, Button } from "react-bootstrap";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  const SignupSchema = Yup.object().shape({
+    // mobile: Yup.string()
+    //   .matches(/^[6-9]\d{9}$/, "Enter a valid 10 Digit Mobile No. ")
+    //   .required("Mobile No. is Mandetory!"),
+    password: Yup.string()
+      .min(6, "Please fill Minimum 6 digit Password !")
+      .max(10, "Invalid Password!")
+      .required("Password is Mandetory!"),
+    email: Yup.string().email("Invalid email").required(" E-mail Id Required!"),
+  });
   return (
     <div>
-      <section>
-            <Container>
-                <Row>
-                    <Col>
-                        Login
-                    </Col>
-                </Row>
-            </Container>
-        </section>
+      <section className="glass-container">
+        <Container>
+          <Row>
+            <Col>
+              <h2>Login</h2>
+              <Formik
+                initialValues={{
+                  // mobile: "",
+                  email: "",
+                  password: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={(values) => {
+                  // same shape as initial values
+                  console.log(values);
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <Row className="input-group">
+                      <Col>
+                        {/* <input type="mail" required> */}
+                        <label>E-Mail</label>
+                      </Col>
+                      <Col>
+                        <Field name="email" type="email" placeholder="email" />
+                        {errors.email && touched.email ? (
+                          <div>{errors.email}</div>
+                        ) : null}
+                      </Col>
+                    </Row>
+                    <Row className="input-group">
+                      <Col>
+                        <label>Password</label>
+                      </Col>
+                      <Col>
+                        <Field
+                          name="password"
+                          type="password"
+                          placeholder="password"
+                        />
+                        {errors.password && touched.password ? (
+                          <div>{errors.password}</div>
+                        ) : null}
+                      </Col>
+                    </Row>
+                    <Row className="form-options">
+                      <Col>
+                        <label>
+                          <input type="checkbox"></input>Remember me{" "}
+                          <a href="/forgot-password" className="forgot-link">
+                            Forgot Password
+                          </a>{" "}
+                        </label>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col></Col>
+                      <Col>
+                        <button type="submit">Login</button>
+                      </Col>
+                    </Row>
+                    <Row className="register">
+                      <Col>
+                        <p>
+                          Don't have an account? <a href="#">register here</a>{" "}
+                        </p>
+                      </Col>
+                    </Row>
+                  </Form>
+                )}
+              </Formik>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
