18a92d945a82445adac432912fcac345bcc7b402
nupurrani <128977360+nupurrani@users.noreply.github.com>
2025-09-20 11:27:20 +0530

    j

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -23,6 +23,49 @@ const AddProduct = () => {
     //   .max(20, "Too Long!")
       .required("Photo is Mandetory!"),
   });
+  const products = [
+    {
+      id: 3,
+      title: "Mens Cotton Jacket",
+      price: 55.99,
+      description:
+        "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+      rating: {
+        rate: 4.7,
+        count: 500,
+      },
+    },
+    {
+      id: 5,
+      title:
+        "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+      price: 695,
+      description:
+        "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+      category: "jewelery",
+      image:
+        "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png",
+      rating: {
+        rate: 4.6,
+        count: 400,
+      },
+    },
+    {
+      id: 9,
+      title: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+      price: 64,
+      description:
+        "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+      category: "electronics",
+      image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png",
+      rating: {
+        rate: 3.3,
+        count: 203,
+      },
+    },
+  ];
   return (
     <div>
       <section className="glass-container">
@@ -116,6 +159,25 @@ const AddProduct = () => {
                   </Form>
                 )}
               </Formik>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col>
+                <div>
+                  {products.map((product,index)=>{
+                    return(
+                      <div>
+                        <p>{product.category}</p>
+                      </div>
+                    )
+                  }
+
+                  )}
+                </div>
             </Col>
           </Row>
         </Container>
